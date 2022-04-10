86753aded5ccedf1a47be1693e41b560c4b7efa1
Sean Gregory <sean.christopher.gregory@gmail.com>
2022-04-10 15:28:43 -0400

    Adds code coverage report
    This MR introduces code coverage report generation with SimpleCov
    As well a couple changes to the library to check if constants are
    defined.

--- tests/arrangment.rb
+++ tests/arrangment.rb
@@ -1,5 +1,13 @@
-require_relative '../src/theorem/hypothesis'
+require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+SimpleCov.start do
+  add_filter '/tests/'
+end
+
 require 'rspec/expectations'
+require_relative '../src/theorem'
 
 module Fixture
   include Theorem::Control::Hypothesis
